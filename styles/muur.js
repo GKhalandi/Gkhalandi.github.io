2926847696adb229e5f701f1b4658d432c2e1a72
Gizing Khalandi <gzing20@me.com>
2019-05-20 14:37:21 +0200

    Aanpassing van css en toevoeging van zandstorm

--- styles/muur.js
+++ styles/muur.js
@@ -1,6 +1,16 @@
 var schuiven = document.querySelector('#muur');
+var storm = document.querySelector('#test');
 
-schuiven.addEventListener('click', function(){
+document.addEventListener('click', function(){
+    console.log("muur schuift weg");
       schuiven.classList.toggle('weg');
+
+});
+
+document.addEventListener('keydown', function(event){
+	if (event.code == 'KeyS') {
+    console.log('zandstorm');
+	   storm.classList.toggle('zandstorm');
+}
 
 });
--- styles/script.js
+++ styles/script.js
@@ -1,6 +1,7 @@
-var hand = document.querySelector('#Hand_Sigaret');
+var hand = document.querySelector('article#Hand_Sigaret > svg:nth-child(1)');
 
 hand.addEventListener('click', function(){
 		console.log("man rookt sigarert");
-	     hand.classList.toggle('schudden');
+	     hand.classList.toggle('roken');
+
 });
