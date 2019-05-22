2ba0afa6950241b0f1c4ccacd9ca2a5edee9cbd7
Gizing Khalandi <gzing20@me.com>
2019-05-22 12:27:18 +0200

    paar aanpassingen
    - responsive elementen maken
    - verplaatsen en verkleuren elementen.
    
    test123 reageert niet, letters en muur reageren wel.

--- styles/script.js
+++ styles/script.js
@@ -1,7 +1,6 @@
 var hand = document.querySelector('#Hand_Sigaret');
 
 hand.addEventListener('mouseover', function(){
-		console.log("man rookt sigarert");
+			console.log("man rookt sigarert");
 	     hand.classList.toggle('test123');
-
 });
